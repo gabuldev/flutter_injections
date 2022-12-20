40b467ba7f4b5af9264c7dc18bf682844d0c215f
Gabul Dev <gabul@Gabuls-MacBook-Air.local>
2022-12-19 23:33:22 -0300

    up

--- doc/docusaurus.config.js
+++ doc/docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 const config = {
   title: "Flutter Injections",
   tagline: "The simple way to manage your dependencies",
-  url: "https://your-docusaurus-test-site.com",
+  url: "https://gabuldev-packages.web.app",
   baseUrl: "/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
@@ -36,15 +36,7 @@ const config = {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          editUrl: "https://github.com/gabuldev/flutter_injections",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
