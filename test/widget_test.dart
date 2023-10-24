8e39ee01b8d759704ec01e1e22b338d69820edc2
Gabul DEV <contato@gabul.dev>
2023-10-24 17:13:04 -0300

    bugfix when change module closed all injections

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 import 'package:flutter_injections/flutter_injections.dart';
 import 'package:flutter_injections/src/exception.dart';
@@ -5,6 +6,20 @@ import 'package:flutter_test/flutter_test.dart';
 
 class TestClass {
   final String name = "name";
+}
+
+class TestClassA {
+  final TestClass testClass;
+  TestClassA({
+    required this.testClass,
+  });
+}
+
+class TestClassB {
+  final TestClass testClass;
+  TestClassB({
+    required this.testClass,
+  });
 }
 
 void main() {
@@ -31,6 +46,63 @@ void main() {
       expect(FlutterInjections.get<TestClass>(), isA<TestClass>());
     });
 
+    testWidgets('disposeAll() disposes once last instance is disposed',
+        (WidgetTester tester) async {
+      final navigatorKey = GlobalKey<NavigatorState>();
+      home() => Builder(
+            builder: (context) {
+              FlutterInjections.get<TestClass>().name;
+              Future.delayed(const Duration(milliseconds: 500), () {
+                Navigator.pushReplacementNamed(context, "/page1");
+              });
+              return Container();
+            },
+          );
+      page1() => FlutterInjectionsWidget(
+            builder: (context) {
+              Future.delayed(const Duration(seconds: 3), () {
+                FlutterInjections.get<TestClassA>().testClass.name;
+                Navigator.pushReplacementNamed(context, "/page2");
+              });
+              return Container();
+            },
+            injections: [
+              Inject<TestClassA>(
+                  (i) => TestClassA(testClass: i.find<TestClass>()))
+            ],
+          );
+      page2() => FlutterInjectionsWidget(
+            builder: (_) {
+              FlutterInjections.get<TestClassB>().testClass.name;
+              return Container();
+            },
+            injections: [
+              Inject<TestClassB>(
+                  (i) => TestClassB(testClass: i.find<TestClass>()))
+            ],
+          );
+      await tester.pumpWidget(FlutterInjectionsWidget(
+          injections: [
+            Inject<TestClass>((_) => TestClass()),
+          ],
+          builder: (context) {
+            return MaterialApp(
+              initialRoute: "/home",
+              navigatorKey: navigatorKey,
+              routes: {
+                "/home": (context) => home(),
+                "/page1": (context) => page1(),
+                "/page2": (context) => page2(),
+              },
+            );
+          }));
+      expect(FlutterInjections.get<TestClass>(), isA<TestClass>());
+      await tester.pumpAndSettle(const Duration(seconds: 1));
+      expect(FlutterInjections.get<TestClassA>(), isA<TestClassA>());
+      await tester.pumpAndSettle(const Duration(seconds: 3));
+
+      expect(FlutterInjections.get<TestClassB>(), isA<TestClassB>());
+    });
     testWidgets('disposeAll() disposes all instances',
         (WidgetTester tester) async {
       await tester.pumpWidget(FlutterInjectionsWidget(
